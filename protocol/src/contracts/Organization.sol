ffa18570eb4bae66c67827c80fca1a2067d01399
Rinaldo <rrossi@byte-code.com>
2016-09-21 18:19:24 +0200

    Add some modifier

--- protocol/src/contracts/Organization.sol
+++ protocol/src/contracts/Organization.sol
@@ -2,6 +2,57 @@ pragma solidity ^0.4.0;
 
 import 'OnlyOwner.sol';
 
+/**@title The Organization Contract */
 contract Organization {
+  /*  
+    //list of all Organization retidield
+    Retifield [] retifields;
+
+    // name of the Organization
+    String name;
+
+    // Balance of Organization
+    uint balance;
+
+    // List of Retifield
+    Retifier [] retifiers;
+
+    // status of Organization
+    State public state = State.EMPTY;
+    */
+
+    // Organization states
+    enum State {
+        CREATED,
+        EMPTY,
+        RETIFIED,
+        REGISTERED,
+        CLOSED
+    }
+
+    // Organization Struct
+    struct OrganizationRequest {
+        String name,
+        String [] retifiers,
+        uint balance,
+        State state = State.CREATED;
+    }
     
+    function Organization () returns (address contractAddress) {
+        nextState();
+        return this.address;
+    }
+
+    //Check the state
+    modifier atState(State _state) {
+        if(state != _state) throw;
+        _;
+    }
+
+    //** @dev move status to next step*/
+    function nextState() internal {
+        state = State(uint(state) + 1);
+    }
+
+    function setBalance();
 }
